af9c2a2616e0fdb83c814fc6ff41562a0fd5e643
Elizabeth Hernández Morales <cyberelizabeth@gmail.com>
2018-08-30 10:11:58 -0500

    HME_Project_commit_00
    Practice
    Project made as a test

---
+++ core/config.php
@@ -0,0 +1,15 @@
+<?php
+	/*Funcionamiento Local
+    const DBHOST_ACCES='localhost';
+    const DBACCES='practica';//training
+    const DBACCES_USER='root';
+    const DBACCES_PASSWORD='root';
+    //Training03
+    */
+    const DBHOST_ACCES	= 'training.cpuv2fdckkwc.us-east-2.rds.amazonaws.com';
+    const DBPORT 		= '3306';
+    const DBACCES		= 'practica';
+    const DBACCES_USER 	= 'root';
+    const DBACCES_PASSWORD 	= 'Training03';
+
+?>
